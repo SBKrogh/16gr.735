c1fe1b96f82714d860dacf61d6b0cd3fef002714
Simon <simonkro@gmail.com>
2016-12-11 20:29:03 +0100

    Measurements

--- Worksheets/Data/Measurement/EndoWrist_Measurements/Force/Matlab/pitch_in.m
+++ Worksheets/Data/Measurement/EndoWrist_Measurements/Force/Matlab/pitch_in.m
@@ -5,8 +5,8 @@ load('pitchdata')
 % data = iddata(y,u,Ts)
 
 ts = 0.02;     %Sample time
-data_x = data.u{1};    % Input
-data_y = data.y{1};    % Output
+data_x = data.u{8};    % Input
+data_y = data.y{8};    % Output
 
 n = length(data_x);  
 for t = 1:n
@@ -16,19 +16,19 @@ end
 
 
 subplot(2,1,2)
-plot(time,data.u{4},time,data.u{2},time,data.u{3})
+plot(time,data.u{2},time,data.u{4},time,data.u{5})
 xlabel('Time [s]')
 ylabel('Current [A]')
 title('Pitch - current')
-%axis([0 30 -2 0.5])
+axis([0 30 -0.5 1])
 grid on
 
 subplot(2,1,1)
-plot(time,data.y{4}(:,2)*1000,time,data.y{2}(:,2)*1000,time,data.y{3}(:,2)*1000)
+plot(time,data.y{2}(:,2)*1000,time,data.y{4}(:,2)*1000,time,data.y{5}(:,2)*1000)
 title('Pitch - upwards force')
 xlabel('Time [s]')
 ylabel('Force [N]')
-%axis([0 30 -0.5 2])
+axis([0 30 -3 0.5])
 grid on
 
 clear figure
